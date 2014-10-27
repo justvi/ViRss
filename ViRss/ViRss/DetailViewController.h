f5116eb4d1f8459f9f57b1766d52b7bb5b38873d
justvi <lhs.ggjy@gmail.com>
2014-10-27 11:57:34 +0800

    basic ui

---
+++ ViRss/ViRss/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  ViRss
+//
+//  Created by lhs on 14/10/27.
+//  Copyright (c) 2014年 lhs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UITableViewController <UISplitViewControllerDelegate>
+
+@end
---
+++ ViRss/ViRss/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  ViRss
+//
+//  Created by lhs on 14/10/27.
+//  Copyright (c) 2014年 lhs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
